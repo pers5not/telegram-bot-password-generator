3dddfd160e040a7ecc8a179acf9260af9ca141ba
pers5not <pers5not@gmail.com>
2022-07-24 16:24:38 +0300

    add get json bot.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import requests
 from pprint import pprint
 
-bot_token = "5582597592:AAEzmOjj06Td3bc5Klld60Da_bie1NTXla4"
+bot_token = ""
 api_link = f"https://api.telegram.org/bot{bot_token}/"
 updates = requests.get(api_link + "getUpdates?offset=-1").json()
 pprint(updates)
 message = updates["result"][0]["message"]
 chat_id = message["from"]["id"]
-text = message["text"]
+text = message['caption']
+print(text)
 
 send_message = requests.get(
     api_link + f"sendMessage?chat_id={chat_id}&text=Привет, ты написал {text}")
